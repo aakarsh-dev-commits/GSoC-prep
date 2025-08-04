eb6ce40c5cdae1689ee443b69f0cb4d4f050489d
Aakarsh Srivastava <138319551+aakarsh-dev-commits@users.noreply.github.com>
2025-08-04 17:46:18 +0530

    Exercise 2 problem#1

--- geo-python/Exercise-2/exercise.py
+++ geo-python/Exercise-2/exercise.py
@@ -38,3 +38,27 @@ print(stationName)
 print(yearOperation)
 
 # Problem 1
+
+temp = [-3.5, -4.0, -1.0, 4.0, 10.0, 14.5, 17.5, 16.0, 11.5, 6.0, 2.0, -1.5]
+month = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+index = input("Enter month index Eg Jan is 0 ")
+
+infoText = (
+    f"The average temperature in Helsinki in {month[int(index)]} is {temp[int(index)]}"
+)
+
+print(infoText)
