145b1779888b20752bcfa9dfc6dc28ae7de5b90b
Aakarsh Srivastava <138319551+aakarsh-dev-commits@users.noreply.github.com>
2025-08-04 17:08:44 +0530

    Exercise 2 problem 0

---
+++ geo-python/Exercise-2/exercise.py
@@ -0,0 +1,40 @@
+# Problem 0
+
+stationName = [
+    "lighthouse",
+    "Harmaja",
+    "Suomenlinna aaltopoiju",
+    "Kumpula",
+    "Kaisaniemi",
+]
+yearOperation = [2003, 1989, 2016, 2005, 1844]
+
+print(stationName)
+print(yearOperation)
+
+
+stationName.append("Alajärvi Möksy")
+yearOperation.append(1957)
+
+print(stationName)
+print(yearOperation)
+
+stationName.append("Alajärvi Möksy")
+yearOperation.append(1957)
+
+print(stationName)
+print(yearOperation)
+
+
+stationName.sort()
+yearOperation.sort()
+
+print(stationName)
+print(yearOperation)
+
+yearOperation.sort()
+
+print(stationName)
+print(yearOperation)
+
+# Problem 1
